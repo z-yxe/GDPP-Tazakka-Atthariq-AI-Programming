8276f149703ac4bb23f583196fce4d3d95a4bce7
TazakkaAtthariq <tazakkaatthariq@gmail.com>
2025-08-29 19:22:13 +0700

    Follow Camera

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -4,21 +4,34 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float speed = 100f;
-
+    [SerializeField]
+    private Camera cam;
     private Rigidbody rb;
 
-    void Awake()
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        HideAndLockCursor();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        Vector3 horizontalDir = horizontalInput * cam.transform.right;
+        Vector3 verticalDir = verticalInput * cam.transform.forward;
+        horizontalDir.y = 0;
+        verticalDir.y = 0;
+
+        Vector3 movementDirection = horizontalDir + verticalDir;
 
         rb.linearVelocity = movementDirection * speed * Time.fixedDeltaTime;
+    }
+
+    private void HideAndLockCursor()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
